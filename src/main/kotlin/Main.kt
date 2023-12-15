dd04efdbb47606b7719cb29642dde6e92a6aef45
BipperTT <alexis.checa.7e7@itb.cat>
2023-12-15 10:37:48 +0100

    main terminado y bien hecho

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,43 @@
+
+
+
+
+
+
+
+
+
 fun main() {
-  for (i in 0..100) {
-      println("Hello World!")
-  }
+
+    println("Benvingut a l'aplicació per calcular el valor de mercat.")
+
+    var model: Int
+    var km: Int
+    var preufinal: Float = 0f
+    do {
+
+        model = menu("Si tens una Volkswagen Grand California escriu 1, si tens una Volkswagen Grand California Full Equip escriu 2.")
+        km = menu("Introdueix el número de km que té el vehicle:")
+
+        if (model == 1) {
+
+
+            var preuinicial:Int = 73490
+            preufinal = precioActual(preuinicial, km)
+
+
+        } else if (model == 2) {
+
+            var preuinicial:Int = 93490
+            preufinal = precioActual(preuinicial, km)
+
+        } else {
+            println("ERROR: Valor introduït incorrecte")
+        }
+
+    } while (model != 1 && model != 2)
+
+    println("El preu final tenint en compte que el vehicle te $km, és de $preufinal€")
+
 }
+
